0e7b49ce634de7eaab9a1deac631b5683b2ead3d
root <root@Shafik.>
2025-05-28 11:36:37 +0200

    ajout de la fonction descente de gradient en 3 étapes

--- train.py
+++ train.py
@@ -14,9 +14,6 @@ def H(v, h, w, theta, eta):
 
     return -(res + v@theta + h@eta)
 
-def p(v, h):
-    pass
-
 
 def sample_ber(p : float):
     x = np.random.rand()
@@ -87,7 +84,7 @@ def estim_emp(v, h):
 
 
 
-def estim_emp_vecteur(mat_v, Ns):
+def estim_empirique_univariee(mat_v, Ns):
     """ 
     Calcule l'estimation emprique de vec ~ P rbm 
     mat_v : (Nvec, Ns)                                             (changer pour que ce soit Ns, Nvec)
@@ -107,8 +104,7 @@ def estim_emp_vecteur(mat_v, Ns):
     return 1/Ns * X # moyenner toutes les composantes 
 
 
-
-def estim_emp_v_h(mat_v, mat_h, Ns):
+def estim_empirique_bivariee(mat_v, mat_h, Ns):
     """ 
     Calcule l'estimation emprique de (v, h) ~ P rbm 
     mat_v : (Ns, Nvec_v)
@@ -129,7 +125,6 @@ def estim_emp_v_h(mat_v, mat_h, Ns):
                 matrice_k[i][a] = mat_v[k][i] * mat_h[k][a] # ses composantes = v_i^(k) * h_a^(k)
 
         matrices_des_k[k] = matrice_k # on va stocker ttes ces matrices k, dans une grosse matrice 
-
 
     for i in range(Nvec_v):
         for a in range(Nvec_h):
@@ -155,14 +150,48 @@ def esperance_data(array_inputs):
             res += array_inputs[i][j]
 
     return 1/(N*D) * res 
-
-def gradient_param(v, h):
-    """ Calcule les gradients des parametres w, eta, theta """
-    grad_w  = estim_emp(v, h)
-    grad_theta = np.mean(v) - np.mean()
-    grad_eta = np.mean(v) - np.mean()
     
 
+def calcul_gradients(mat_v, mat_h, Ns, inputs_data):
+    """ Calcule les gradients des parametres w, eta, theta """ 
+    grad_w     = esperance_data(inputs_data) - estim_empirique_bivariee(mat_v, mat_h, Ns)  # <viha>_D - <viha>_RBM
+    grad_theta = np.mean(mat_v) - estim_empirique_univariee(mat_v, Ns)                     # <vi>_D - <vi>_RBM
+    grad_eta   = np.mean(mat_h) - estim_empirique_univariee(mat_h, Ns)                     # <ha>_D - <ha>_RBM
+
+    return (grad_w, grad_theta, grad_eta)
+
+
+def descente_gradient_rbm(h, v, Ns, w0, eta0, theta0, nstep=10, mu):
+    """
+    Met à jour les gradients des parametres w, eta, theta du modèle RBM 
+    mu : taux d'apprentissage 
+    Ns : nombre d'échantillons 
+    les valeurs initiales des parametres sont définies avant.
+    """
+    mat_h = np.zeros((h, Ns))
+    mat_v = np.zeros((v, Ns))
+    
+    w = w0
+    eta = eta0
+    theta = theta0
+
+    for i in range (nstep):
+        # 1ere etape : echantillonage
+        for k in range(Ns):
+            mat_h[k], mat_v[k] = bgs(w, eta, theta, nstep) # ie. (couche_h, couche_v), a chaque fois nouveau
+
+        # 2eme etape : calcul gradient 
+        grad_w, grad_eta, grad_theta = calcul_gradients(mat_v, mat_h, Ns)
+
+        # 3eme etape : MAJ du gradient
+        w = w + mu * grad_w
+        eta = eta + mu * grad_eta
+        theta = theta + mu * grad_theta
+        
+    return w, eta, theta
+
+    
+     
 
 if __name__ == "__main__":
     """
@@ -208,7 +237,7 @@ if __name__ == "__main__":
     # Dimensions de inputs: (N, D) 
     inputs_np = np.array(inputs)
     print(inputs_np.shape) 
-    nb_samples = inputs_np.shape[0] # N
+    nb_samples = inputs_np.shape[0] # N 
     nb_pixels = inputs_np.shape[1]  # D
 
     esperance_data = esperance_data(inputs_np)
@@ -216,16 +245,3 @@ if __name__ == "__main__":
 
     # 4. Calcul des gradients de w, eta, theta 
     (grad_w, grad_eta, grad_theta) = gradient_param()
-
-
-
-
-
-
-
-
-
-
-
-
-
